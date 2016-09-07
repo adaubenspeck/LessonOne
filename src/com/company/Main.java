c088333a3348a81057559efe7e7d638b3ef00fb4
Andrew R Daubenspeck <ad110@pvhs-lab112-d06.pvsd.org>
2016-09-07 14:22:15 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.print("Hello World");
+        System.out.println("Whats up");
+        //the above is the two ways we need to
+        // know how to output text
+        //two tupes of coments
+        //Line comments //
+        //Block commment /*
+        System.out.print(87);
+        System.out.print(1.0);
+    }
+}
